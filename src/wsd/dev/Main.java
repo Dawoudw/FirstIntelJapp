0eab3b187e258ed146cd4d26adba26c3425d4edc
Admin <wsd81_2@hotmail.com>
2020-11-26 02:12:38 -0500

    test update

--- src/wsd/dev/Main.java
+++ src/wsd/dev/Main.java
@@ -5,7 +5,7 @@ import wsd.dev.entities.TestA;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+ 
         TestA t = new TestA();
         System.out.println(t.getClass().getClassLoader().getName());
         System.out.println("Hi Wael " + "Dawoud" + " ");

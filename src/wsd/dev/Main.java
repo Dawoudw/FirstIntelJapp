03fa5d3a811e5b2cb1ac4fe62195669565702893
Admin <wsd81_2@hotmail.com>
2020-11-26 02:24:11 -0500

    test update

--- src/wsd/dev/Main.java
+++ src/wsd/dev/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
  //Do some changes
+        int x =0;
         TestA t = new TestA();
         System.out.println(t.getClass().getClassLoader().getName());
         System.out.println("Hi Wael " + "Dawoud" + " ");
